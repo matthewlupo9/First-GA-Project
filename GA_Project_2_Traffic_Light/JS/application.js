b38ae855d7dcd483555b978f364245676f55eb2c
matthewlupo9 <phlchen@Peter-Chens-MacBook-Pro.local>
2014-09-30 22:10:23 -0400

    New Updates to JS for Traffic Light

--- GA_Project_2_Traffic_Light/JS/application.js
+++ GA_Project_2_Traffic_Light/JS/application.js
@@ -2,6 +2,7 @@ function buttonClick(){
   var greenLight = document.getElementById("green");
   var yellowLight = document.getElementById("yellow");
   var redLight = document.getElementById("red");
+ }
   
   greenLight.style.backgroundColor = "green";
   yellowLight.style.backgroundColor = "yellow";
