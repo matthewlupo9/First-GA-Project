20e6c486e35b629640d5bb53cc3a62dc394ed6f7
Saimon Sharif <sharif.saimon@gmail.com>
2014-10-05 20:16:47 -0400

    Feedback for photo album

--- GA_Project_3_Photo_Album/JS/application.js
+++ GA_Project_3_Photo_Album/JS/application.js
@@ -1,4 +1,4 @@
-var images = ["img/photo_0.jpg", "img/photo_1.jpg", "img/photo_2.jpg", "img/photo_3.jpg", "img/photo_4.jpg", "img/photo_5.jpg", "img/photo_6.jpg"]
+var images = ["img/photo_0.jpg", "img/photo_1.jpg", "img/photo_2.jpg", "img/photo_3.jpg", "img/photo_4.jpg", "img/photo_5.jpg", "img/photo_6.jpg"];
 var currentImage = 0;
 
 function switchPhoto(choose) {
@@ -12,7 +12,7 @@ function nextPhoto() {
 	if(currentImage===images.length){
 		currentImage = 0;
 	}
-	var img = document.getElementById('mainPicture');
+	var img = document.getElementById('mainPicture'); // You're using this same variable in 3 functions. If you made it a global variable, it would help condense your code.
 	img.setAttribute('src', images[currentImage]);
 }
 
