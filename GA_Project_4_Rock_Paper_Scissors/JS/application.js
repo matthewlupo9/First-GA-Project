b97cc7cc2d82ea0faa2abb8d105266db3a680aa4
matthewlupo9 <phlchen@Peter-Chens-MacBook-Pro.local>
2014-10-12 12:25:28 -0400

    Rock Paper Scissors Final

--- GA_Project_4_Rock_Paper_Scissors/JS/application.js
+++ GA_Project_4_Rock_Paper_Scissors/JS/application.js
@@ -1,27 +1,3 @@
-// var playerChoice = ["img/rock.jpg", "img/paper.jpg", "img/scissors.jpg"]
-
-// var computerChoice = ["rock", "paper", "scissors"]
-
-// var allWin = ["paperRock", "rockScissors", "scissorsPaper"]
-
-// var allLose = ["rockPaper", "scissorsRock", "paperScissors"]
-
-// var whoWins = 
-
-// function randomChoice(pick) {
-// 	if (var playerChoice(0) = var computerChoice(0)) {
-// 		alert("draw");
-// 	}
-
-// 	if (var playerChoice(1)) {
-// 		var computerChoice (0,1,2);
-// 	}
-
-// 	if (var playerChoice(2)) {
-// 		var computerChoice (0,1,2);
-// 	}
-// }
-
 function gameStart(index) {
 	var choices = ["rock", "paper", "scissors"];
 	var userChoice = choices[index];
